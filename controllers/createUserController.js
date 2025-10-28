552074165288a4276dbc3c0b2a0f918f7388456c
Jasminder Singh <jassdhiman958@gmail.com>
2025-10-28 18:15:36 +0530

    added referral feature

--- controllers/createUserController.js
+++ controllers/createUserController.js
@@ -4,23 +4,52 @@ import { userModel } from "../lib/database/userSchema.js";
 export const createUserController = async (req, res) => {
     try {
         await connectDB();
-        const { fullName, email, userId } = req.body;
+        const { fullName, email, userId, referral } = req.body;
         // First check if user already crated.
-        
-        const isAlreadyCreated = await userModel.findOne({ email : email.emailAddress }).populate('campaigns');
-    
+
+        const isAlreadyCreated = await userModel.findOne({ email: email.emailAddress }).populate('campaigns');
+
         if (isAlreadyCreated) {
             res.status(409).json({ message: "Account is already created.", user: isAlreadyCreated });
         } else {
+            // if thi user is referred by someone then that user should get 5000 points.
+            if (referral) {
+                await userModel.findOneAndUpdate(
+                    { referralCode: referral },
+                    {
+                        $inc: {
+                            referralPointsEarned: 5000,
+                            referralCount: 1,
+                            currentPoints: 5000,
+                            todayEarned: 5000
+                        },
+                        $push: {
+                            notifications: {
+                                message: "Congrats! You’ve earned 5000 points through a referral!",
+                                notificationDate: new Date(),
+                                isRead: false,
+                            }
+                        }
+                    }, { new: true })
+            }
             const user = await userModel.create({
                 userId,
-                email: emailAddress,
+                email: email.emailAddress,
                 name: fullName,
                 recentActivites: [],
                 campaigns: [],
                 dailyTasks: [],
-                notifications: [],
-                currentPoints: 3000
+                notifications: referral ? [{
+                    message: "Congrats! You’ve earned 5000 points for joining through a referral!",
+                    notificationDate: new Date(),
+                    isRead: false,
+                },] : [],
+                currentPoints: referral ? 3000 + 5000 : 3000,
+                referralPointsEarned: referral ? 5000 : 0,
+                referralCount: 0,
+                todayEarned: 5000,
+                referralCode: email.emailAddress.split('@')[0]
+
             });
 
             res.status(201).json({ message: 'User Created Successfully.', user: user });
